8d2e9178a78fd6ce21401d789cdc6541a8c42863
pankaj <pankajthekush@gmail.com>
2020-07-20 18:05:21 +0530

    update

--- bowser/bowser.py
+++ bowser/bowser.py
@@ -53,7 +53,7 @@ class LinkCheck():
 
         crl.setopt(pycurl.HTTPHEADER,["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                                 "Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-                                "Accept-Encoding: deflate",
+                                "Accept-Encoding: gzip, deflate, br",
                                 "Connection: keep-alive"])        
         
         try:
@@ -80,37 +80,41 @@ class LinkCheck():
                 decompressed_data=zlib.decompress(storege.getvalue(), 16+zlib.MAX_WBITS)
                 html  = decompressed_data
                
-            except Exception:
+            except Exception as e:
                 self.return_data['input_url'] = self.wbstring
                 self.return_data['output_url'] = 'error'
                 self.return_data['body'] = 'error'
                 self.return_data['status-code'] ='error'
+                self.return_data['error-comment'] = str(e)
                 return
         elif encoding == 'br':
             try:
                 decompressed_data = brotli.decompress(storege.getvalue())
                 html  = decompressed_data
-            except Exception:
+            except Exception as e:
                 self.return_data['input_url'] = self.wbstring
                 self.return_data['output_url'] = 'error'
                 self.return_data['body'] = 'error'
                 self.return_data['status-code'] ='error'
+                self.return_data['error-comment'] = str(e)
                 return
             
         elif encoding == 'deflate':
             try:
                 html  = storege.getvalue()
-            except Exception:
+            except Exception as e:
                 self.return_data['input_url'] = self.wbstring
                 self.return_data['output_url'] = 'error'
                 self.return_data['body'] = 'error'
                 self.return_data['status-code'] ='error'
+                self.return_data['error-comment'] = str(e)
                 return
         else:
             self.return_data['input_url'] = self.wbstring
             self.return_data['output_url'] = 'error'
             self.return_data['body'] = 'error'
             self.return_data['status-code'] ='error'
+            self.return_data['error-comment'] = 'invalid content type'
             return
 
         visible_html = text_from_html(html)
@@ -121,11 +125,6 @@ class LinkCheck():
         self.return_data['status-code'] =st_code
 
         
-
-
-
-
-
 
 
 def tag_visible(element):
@@ -159,14 +158,81 @@ def parse_url(url):
     return dict_result
 
 
-def create_data_set(input_url,output_url=None,url_body=None):
-    dict_input_url = parse_url(input_url)
-    dict_output_url = parse_url(output_url)
+class UrlChecker():
+    def __init__(self,input_url,output_url=None,url_body=None,status_code=None):
+
+        self.ucheck_result = dict()
+
+        self.input_url = input_url
+        self.output_url = output_url
+        self.url_body = url_body
+        self.status_code = status_code
+
+
+        
+        self.input_url_schema = None
+        self.input_url_netloc = None
+        self.input_url_path = None
+        self.input_url_params = None
+        self.input_url_query = None
+        self.input_url_fragment = None
+
+
+        self.output_url_schema = None
+        self.output_url_netloc = None
+        self.output_url_path = None
+        self.output_url_params = None
+        self.output_url_query = None
+        self.output_url_fragment = None
+
+        if self.output_url is None:
+            lc = LinkCheck(self.input_url)
+            dict_url_data = lc.return_data
+            self.output_url = dict_url_data['output_url']
+            self.url_body = dict_url_data['body']
+            self.status_code = dict_url_data['status_code']
+
+    def set_parsed_url_data(self):
+        dict_input_url = parse_url(self.input_url)
+        dict_output_url = parse_url(self.output_url)
+
+         
+        self.input_url_schema = dict_input_url['scheme']
+        self.input_url_netloc = dict_input_url['netloc']
+        self.input_url_path = dict_input_url['path']
+        self.input_url_params = dict_input_url['params']
+        self.input_url_query = dict_input_url['query']
+        self.input_url_fragment = dict_input_url['fragment']
+
+
+
+        self.output_url_schema = dict_output_url['scheme']
+        self.output_url_netloc = dict_output_url['netloc']
+        self.output_url_path = dict_output_url['path']
+        self.output_url_params = dict_output_url['params']
+        self.output_url_query = dict_output_url['query']
+        self.output_url_fragment = dict_output_url['fragment']
+
+    def compare_url(self):
+        #if output url is not good 
+        if self.output_url_schema is None or len(self.output_url_schema <=2):
+            self.ucheck_result['input_url'] = self.input_url
+            self.ucheck_result['output_url'] = self.output_url
+            self.ucheck_result['status_code'] = self.status_code
+            
+            
+
+
+        
+            
+        
+
+
 
     
 
 
 
 if __name__ == "__main__":
-    lc = LinkCheck('google.com')
+    lc = LinkCheck('https://www.cloudsavvyit.com/1046/reduce-your-websites-size-with-gzip-and-deflate-compression/')
     print(lc.return_data)
